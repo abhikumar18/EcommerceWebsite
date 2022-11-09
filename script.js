255bd94356e89150c4c1a1439e85f90117ddd8e2
Abhishek Kumar <abhishek.it.msit@gmail.com>
2022-11-09 21:58:49 +0000

    Search Bar Logic

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ if(close){
     })
 }
 
+// Search Bar Logic
 
+const searchProducts = ()=>{
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("pro-container");
+    const product = document.querySelectorAll(".pro");
+    const pname = storeitems.getElementsByTagName("h5");
+
+    for(var i=0; i < pname.length;i++){
+        let match = product[i].getElementsByTagName('h5')[0];
+
+        if(match){
+           let textvalue =  match.textContent || match.innerHTML
+
+           if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+            product[i].style.display = "";
+           }
+           else{
+            product[i].style.display = "none";
+           }
+        }
+    }
+}
